f12854917eef875ebb65044f924186c975b7f197
Muhammadibrahim90 <ibrahim968712@gmail.com>
2025-04-16 02:18:03 -0700

    Product store

--- checkcard.js
+++ checkcard.js
@@ -1,21 +1,21 @@
 
-  const cartContainer = document.getElementById("cartContainer");
-  const totalAmount = document.getElementById("totalAmount");
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+const cartContainer = document.getElementById("cartContainer");
+const totalAmount = document.getElementById("totalAmount");
+const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-  let total = 0;
+let total = 0;
 
-  if (cart.length === 0) {
+if (cart.length === 0) {
     cartContainer.innerHTML = "<p class='text-center text-gray-500 col-span-3'>Your cart is empty.</p>";
-    totalAmount.textContent = "Your Cart";
-  } else {
+    totalAmount.textContent = "";
+} else {
     cart.forEach(product => {
-      total += product.price;
+        total += product.price;
 
-      const card = document.createElement("div");
-      card.className = "bg-white p-4 rounded-lg shadow-md space-y-2";
+        const card = document.createElement("div");
+        card.className = "bg-white p-4 rounded-lg shadow-md space-y-2";
 
-      card.innerHTML = `
+        card.innerHTML = `
         <img src="${product.image}" alt="${product.title}" class="h-40 w-full object-contain">
         <h2 class="font-semibold text-lg">${product.title}</h2>
         <p class="text-green-600 font-bold">$${product.price}</p>
@@ -23,19 +23,19 @@
           Remove
         </button>
       `;
-      cartContainer.appendChild(card);
+        cartContainer.appendChild(card);
     });
 
- 
-    totalAmount.textContent = `Total: $${total.toFixed(2)}`;
-  }
 
- 
-  cartContainer.addEventListener("click", function (e) {
+    totalAmount.textContent = `Total: $${total.toFixed(2)}`;
+}
+
+
+cartContainer.addEventListener("click", function (e) {
     if (e.target.classList.contains("remove-btn")) {
-      const idToRemove = parseInt(e.target.dataset.id);
-      const newCart = cart.filter(p => p.id !== idToRemove);
-      localStorage.setItem("cart", JSON.stringify(newCart));
-      location.reload();
+        const idToRemove = parseInt(e.target.dataset.id);
+        const newCart = cart.filter(p => p.id !== idToRemove);
+        localStorage.setItem("cart", JSON.stringify(newCart));
+        location.reload();
     }
-  });
+});
